c516c9c954c1a1a959c1383a8d4014f5f0d1f028
alaamerhebb <alaamerhebb@gmail.com>
2021-12-13 13:58:31 +0200

    fixed-date

--- answers.sh
+++ answers.sh
@@ -1,31 +1,31 @@
-1. Get all the names of students in the database
+# 1. Get all the names of students in the database
   select name from students 
 
-2. Get all the data of students above 30 years old
+# 2. Get all the data of students above 30 years old
   select * from students WHERE Age>30
 
-3.Get the names of the females who are 30 years old
+# 3.Get the names of the females who are 30 years old
   select name from students WHERE Gender="F" and Age=30
 
-4. Get the number of Points of Alex 
+# 4. Get the number of Points of Alex 
   select Points from students where name="Alex"
 
-5.Add yourself as a new student (your name, your age...)
+# 5.Add yourself as a new student (your name, your age...)
   insert into students (name,age,gender,points) values ('alaa','22','m','600');
 
-6. Increase the points of Basma because she solved a new exercise.
+# 6. Increase the points of Basma because she solved a new exercise.
   update students
   set points='400'
   where name='Basma';
 
-7. Decrease the points of Alex because he's late today.
+# 7. Decrease the points of Alex because he's late today.
   update students
   set points='150'
   where name='Alex';
 
-Creating Table:
+# Creating Table:
 
-. Create a table graduates:
+# . Create a table graduates:
 
   CREATE TABLE Graduates (
      ID Integer Not Null Primary key AUTOINCREMENT,
@@ -36,63 +36,63 @@ Creating Table:
      Graduation Text
 	 );
 
-.Copy Layal's data from students to graduates:
+# .Copy Layal's data from students to graduates:
 
     INSERT INTO Graduates (Name, Age, Gender, Points)
     SELECT Name, Age, Gender, Points FROM students
     WHERE ID=4;    
 
-.Add the graduation date previously mentioned to Layal's record in graduates:
+# .Add the graduation date previously mentioned to Layal's record in graduates:
 
     update Graduates
-    set Graduation='08/09/2018'
+    set Graduation='2018-09-08'
     where ID='9';
 
-.Remove Layal's record from students:
+# .Remove Layal's record from students:
 
     DELETE FROM students WHERE ID='4'
 
 
-Joins:
+# Joins:
 
-1.Produce a table that contains, for each employee, his/her name, company name, and company date.
+# 1.Produce a table that contains, for each employee, his/her name, company name, and company date.
 
    CREATE TABLE experience AS
    SELECT employees.Name, companies.Name, companies.Date
    FROM employees, companies
    WHERE employees.Company = companies.Name;
 
-2.Find the name of employees that work in companies made before 2000.
+# 2.Find the name of employees that work in companies made before 2000.
   
    select Name from experience WHERE Date<2000
 
-3.Find the name of company that has a graphic designer.
+# 3.Find the name of company that has a graphic designer.
 
    select name from employees where Role='Graphic Designer'
 
 
-Count & Filter:
+# Count & Filter:
 
-1.Find the person with the highest number of points in students
+# 1.Find the person with the highest number of points in students
 
    SELECT Name From Students WHERE Points = (SELECT MAX(Points) FROM Students)
 
 
-2.Find the average of points in students
+# 2.Find the average of points in students
 
    SELECT AVG(points) from students
 
 
-3.Find the number of students that have 500 points
+# 3.Find the number of students that have 500 points
 
    SELECT COUNT(name) FROM students WHERE Points = 500
 
 
-4.Find the names of students that contains 's'
+# 4.Find the names of students that contains 's'
 
    SELECT Name FROM students WHERE Name LIKE '%s%'
 
 
-5.Find all students based on the decreasing order of their points
+# 5.Find all students based on the decreasing order of their points
 
    SELECT * FROM students ORDER by  Points DESC
